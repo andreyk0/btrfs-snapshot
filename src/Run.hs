0334c01c95a45a2cb19bd9d681247d3ea16c4094
Andrey Kartashov <andrey.kartashov@gmail.com>
2019-11-24 12:18:04 -0500

    entry points

--- src/Run.hs
+++ src/Run.hs
@@ -1,10 +1,35 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module Run (run) where
 
-import Import
-import Types
+import           Import
+import           Types
 
-run :: RIO App ()
+run
+  :: HasCLIOptions env
+  => HasLogFunc env
+  => RIO env ()
 run = do
-  logInfo "We're inside the application!"
+  cmd <- view cliCommand
+  case cmd
+    of CLISnapshotCreate c -> runCLISnapshotCreate c
+       CLISnapshotTransfer t -> runCLISnapshotTransfer t
+
+
+runCLISnapshotCreate
+  :: HasCLIOptions env
+  => HasLogFunc env
+  => CLISnapshotCreate
+  -> RIO env ()
+runCLISnapshotCreate (SnapshotCreate paths) = do
+  logInfo . display $ tshow paths
+
+
+runCLISnapshotTransfer
+  :: HasCLIOptions env
+  => HasLogFunc env
+  => CLISnapshotTransfer
+  -> RIO env ()
+runCLISnapshotTransfer (SnapshotTransfer tPathFrom tPathTo) = do
+  logInfo . display $ tshow (tPathFrom, tPathTo)
